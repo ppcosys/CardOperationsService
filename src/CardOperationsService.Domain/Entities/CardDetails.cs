cd452fd714d73863babebe45eae000f5bbdc9ca1
Peter <ppcosys@gmail.com>
2025-11-18 21:26:26 +0100

    Add CardDetails entity  to Domain Entities

---
+++ src/CardOperationsService.Domain/Entities/CardDetails.cs
@@ -0,0 +1,16 @@
+﻿using CardOperationsService.Domain.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CardOperationsService.Domain.Entities
+{
+    public record CardDetails(
+        string CardNumber,
+        CardType CardType,
+        CardStatus CardStatus,
+        bool IsPinSet
+    );
+}
