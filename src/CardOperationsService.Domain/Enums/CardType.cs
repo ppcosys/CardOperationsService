55079fa1797149330815cd59110d9a5795e5555e
Peter <ppcosys@gmail.com>
2025-11-18 21:08:19 +0100

    Add CardType enum to Domain Enums

---
+++ src/CardOperationsService.Domain/Enums/CardType.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CardOperationsService.Domain.Enums
+{
+    public enum CardType
+    {
+        Prepaid,
+        Debit,
+        Credit
+    }
+}
