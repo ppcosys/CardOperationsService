ca2cb95c6b98adde77783999e70aeeb49a3ac8fa
Peter <ppcosys@gmail.com>
2025-11-18 21:13:12 +0100

    Add CardStatus enum to Domain Enums

---
+++ src/CardOperationsService.Domain/Enums/CardStatus.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CardOperationsService.Domain.Enums
+{
+    public enum CardStatus
+    {
+        Ordered,
+        Inactive,
+        Active,
+        Restricted,
+        Blocked,
+        Expired,
+        Closed
+    }
+}
